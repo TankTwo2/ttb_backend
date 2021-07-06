dce084e63f16bbcee791737f5b61336e26ba0e04
Tanktwo2 <rhkdtjr9@naver.com>
2021-07-07 01:21:01 +0900

    [ttb-002] maria db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteController } from './note/note.controller';
 import { NoteService } from './note/note.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController, NoteController],
   providers: [AppService, NoteService],
 })
