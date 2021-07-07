5c28e94961d500cde57da7aea6222af79a97f521
Tanktwo2 <rhkdtjr9@naver.com>
2021-07-08 00:29:00 +0900

    [tts-002] dev 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,28 @@ import { AppService } from './app.service';
 import { NoteController } from './note/note.controller';
 import { NoteService } from './note/note.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../secret_config.json';
+
+const env = process.env.NODE_ENV;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      envFilePath: env === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: env === 'prod',
     }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.host,
+      port: config.port,
+      username: config.user,
+      password: config.password,
+      database: env === 'dev' ? 'TTS_DATABASE_DEV' : 'TTS_DATABASE',
+      synchronize: true,
+      logging: false,
+      entities: ['src/entity/**/*.ts'],
+    }),
   ],
   controllers: [AppController, NoteController],
   providers: [AppService, NoteService],
