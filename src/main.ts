c405d0740df35fa1bce6bfdc449f958e0f5bcbda
Tanktwo2 <rhkdtjr9@naver.com>
2021-07-14 16:43:24 +0900

    [ttb-003] : cors test

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const whitelist = [
-    'http://localhost:3000',
-    'https://tanktwo2.github.io',
-    'http://192.168.0.6',
-  ];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(4001);
 }
 bootstrap();
